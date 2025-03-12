50d2c1c3caa6b824a649f83a917f648cf6dcd11c
rovior <robertbarbosabarros@gmail.com>
2025-03-12 13:43:03 -0300

    feat:implementando corpo a home do projeto e atribuindo o botão de criar um novo projeto, jundo a imagem e a implementaçáo do css a home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
@@ -16,15 +16,12 @@ function App() {
       <Navbar/>
       <Container customClass="min-height">
       <Routes>
-  
-
         <Route path="/" exact element={<Home />} />
         <Route path="/company" exact element={<Company />} />
         <Route path="/contact" exact element={<Contact />} />
         <Route path="/projects" exact element={<Projects />} />
         <Route path="/navbar" exact element={<Navbar />} />
-        <Route path="/newproject" exact element={<NewProject />} />
-        
+        <Route path="/NewProject" element={<NewProject />} />
       </Routes>
       </Container>
       
